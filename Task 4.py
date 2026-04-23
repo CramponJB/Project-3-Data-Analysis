82e3b32d53f696c5f09a020e701d976699daa1f4
CramponJB <150046291+CramponJB@users.noreply.github.com>
2026-04-23 20:00:41 +0200

    final task 4

--- Task 4.py
+++ Task 4.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 16:18:11 2026
+
+@author: jeanb
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -14,8 +21,8 @@ dy=0.3
 Nx = round(Lx/dx)
 Ny = round(Ly/dy)
 
-dt=0.1
-tfinal=3000
+dt=0.15
+tfinal=10000
 Nt=int(tfinal//dt)
 
 T0=10
@@ -29,6 +36,8 @@ x0, y0 = borehole
 ix = int(x0 / dx)
 iy = int(y0 / dy)
 
+bc=[0,0,0,0] # border (right, up, left, down)
+
 def vx(x):
     return 0.02*(x +20)+0.01 
 
@@ -41,7 +50,7 @@ y_coords = np.arange(Ny) * dy
 vx_field = vx(x_coords)[:, None]
 vy_field = vy(y_coords)[None, :]
 
-K = alpha*dt/(dx*dy)
+K = alpha*dt/(dx*dx)
 vx2 = vx_field*dt/dx
 vy2 = vy_field*dt/dy
 
@@ -50,9 +59,11 @@ vx_neg = np.minimum(vx2, 0)
 vy_pos = np.maximum(vy2, 0)
 vy_neg = np.minimum(vy2, 0)
 
-Q= 0.15*np.random.randn(Nt)
+Q= 0.3*np.random.randn(Nt)
 
 ### Initialisation
+target_times = [2000, 5000, 10000]
+results = []
 
 Told=np.zeros((Nx,Ny))+T0
 T=np.zeros((Nx,Ny))
@@ -82,6 +93,9 @@ for k in range(Nt):
 
     Told=T.copy()
 
+    if int(k*dt) in target_times:
+        results.append(T.copy())    
+
     if k%100==0:
         print(k*dt)
         #print(T)
@@ -102,20 +116,32 @@ ax.set_ylabel('y')
 plt.show()
 """
 ###     Echelle de couleur logarithmique
+import numpy as np
 
-#Creating vexctors with the x and y values of the grid and plotting the result
-x=np.linspace(0,Lx,Nx)
-y=np.linspace(0,Ly,Ny)
+x = np.linspace(0, Lx, Nx)
+y = np.linspace(0, Ly, Ny)
 Y, X = np.meshgrid(y, x)
 
-fig, ax=plt.subplots() 
-surf = ax.contourf(X, Y, T,
-                   levels=100,
-                   norm=LogNorm(vmin=T.min()+1e-6, vmax=T.max()),
-                   cmap=cm.coolwarm)
-bar=fig.colorbar(surf, shrink=0.5, aspect=5)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-plt.show()
+titles = ["t = 2 000 s", "t = 5 000 s", "t = 10 000 s"]
 
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+for i, t_val in enumerate(target_times):
+    dT = results[i] - T0
+    surf = axes[i].contourf(X, Y, dT,
+                            levels=15,
+                            cmap='coolwarm')
+    axes[i].set_title(f't = {t_val} s')
+    axes[i].set_xlabel('x [m]')
+    axes[i].set_ylabel('y [m]')
+cbar = fig.colorbar(surf, ax=axes.ravel().tolist(),
+                    orientation='horizontal',
+                    pad=0.21,
+                    shrink=0.9)
+
+cbar.set_label('T - T₀ [°C]')
+cbar.ax.xaxis.set_label_position('top')
+cbar.ax.xaxis.set_ticks_position('top')
+plt.tight_layout()
+plt.show()
 
